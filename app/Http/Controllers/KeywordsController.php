acaa4974abb9257b7acc49ab027d9ba111cc99b2
Adam Pietrzykowski <eresanto@gmail.com>
2017-09-06 16:55:02 +0200

    add pluck method

--- app/Http/Controllers/KeywordsController.php
+++ app/Http/Controllers/KeywordsController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Keyword;
 
 class KeywordsController extends Controller
 {
     public function index()
     {
-        $keywords =  App\Keyword::all()
-        return view('keywords.index', compact('$keywords'))
+        $keywords =  Keyword::get()->pluck('keyword');
+        return view('keywords.index', compact('keywords'));
     }
 
 }
--- resources/views/keywords/index.blade.php
+++ resources/views/keywords/index.blade.php
@@ -1,1 +1,14 @@
-Stwórz
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+<ul>
+@foreach ($keywords as $keyword)
+    <li>{{ $keyword}}</li>
+@endforeach
+</ul>
+    </body>
+</html>
